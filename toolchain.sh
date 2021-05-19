2d2346d8e0131ee952550b5f9a7bee137650767d
7dog123 <futuretrunks745@gmail.com>
2021-05-19 02:12:22 -0500

    build the tarballs directory

--- toolchain.sh
+++ toolchain.sh
@@ -9,6 +9,9 @@ mkdir -p build || { echo "ERROR: Could not create the build directory."; exit 1;
 ## Enter the build directory.
 cd build || { echo "ERROR: Could not enter the build directory."; exit 1; }
 
+## Create the tarballs directory.
+mkdir -p tarballs || { echo "ERROR: Could not create the build directory."; exit 1; }
+
 ## Fetch the depend scripts.
 DEPEND_SCRIPTS=(`ls ../depends/*.sh | sort`)
 
